bfa587adda6af464ba58245ba9422a291d1f05bf
kimwz <kimwz.kr@gmail.com>
2025-05-14 08:44:38 +0900

    refactor: Remove previous code/tool results from the prompts because Gemini responds to them as they are.

--- app/lib/.server/llm/stream-text.ts
+++ app/lib/.server/llm/stream-text.ts
@@ -47,8 +47,8 @@ export async function streamText(props: {
         if (part.type === 'text') {
           part.text = part.text.replace(/<div class=\\"__boltThought__\\">.*?<\/div>/s, '');
           part.text = part.text.replace(/<think>.*?<\/think>/s, '');
-          part.text = part.text.replace(/(<boltAction[^>]*>)([\s\S]*?)(<\/boltAction>)/gs, '$1(truncated)$3');
-          part.text = part.text.replace(/(<toolResult[^>]*>)([\s\S]*?)(<\/toolResult>)/gs, '$1(truncated)$3');
+          part.text = part.text.replace(/<(boltAction[^>]*)>([\s\S]*?)(<\/boltAction>)/gs, '');
+          part.text = part.text.replace(/<(toolResult[^>]*)>([\s\S]*?)(<\/toolResult>)/gs, '');
         }
       }
 
